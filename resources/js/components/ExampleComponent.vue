726432965345cd0d8366fc6c03cb9dbf1ea92f0c
DESKTOP-K05ILPQ\user <criswork666@gmail.com>
2022-10-29 21:37:55 +0800

    webpack5 error deal

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,16 +1,6 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">aaaa</div>
 
-                    <div class="card-body">
-                        <hr>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
