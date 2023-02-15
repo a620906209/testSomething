984d40044a530ee890911dcea5190595f4f22ac7
Hank <a6209123456789@yahoo.com.tw>
2023-02-15 16:17:29 +0800

    test_fun

---
+++ app/Http/Controllers/funController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class funController extends Controller
+{
+    //
+    public function fun(){
+        return "fun";
+    }
+}
